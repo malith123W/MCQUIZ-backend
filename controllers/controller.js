fb51a7ded7d2ca8296815896a451de04111c2bb6
Duleni Wickramasinghe <duleniunieng@gmail.com>
2025-05-20 20:37:36 +0530

    Adding some more options to registration

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,31 @@ const User = require('../models/model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists with this email' });
+    }
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      role: 'user'
+    });
+
+    await newUser.save();
+    
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ message: 'Server error during registration' });
+  }
+};
 
 const loginUser = async (req, res) => {
   try {
