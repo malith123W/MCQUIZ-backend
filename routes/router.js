639b4fd256062a577229b256bd8afa4cb3bc17ba
Ashan Kasthuriarachchi <145893442+ashan2001ruh@users.noreply.github.com>
2025-08-24 19:46:28 +0530

    Troubleshooting

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/controller');
 
 router.post('/register', controller.registerUser);
-router.post('/login', controller.loginUser);
+router.post('/api/login', controller.loginUser);
 
 module.exports = router;
